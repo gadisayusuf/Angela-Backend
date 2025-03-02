4cda347ae79a956a11d27507aff2cc7c9939a821
Gadisa Yusuf Aliyi <gadisa672@gmail.com>
2025-03-02 11:21:29 +0300

    types of authentication with axios requests

---
+++ 5.4 API Authentication/index.js
@@ -0,0 +1,93 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/";
+
+//TODO 1: Fill in your values for the 3 types of auth.
+const yourUsername = "gadisa";
+const yourPassword = "@gadisa";
+const yourAPIKey = "386a2873-35ee-4623-b092-783d51de0384";
+const yourBearerToken = "3182d35c-8c98-4e9b-bb02-21add9dd0f79";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(`${API_URL}random`);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}all?page=2`, {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error fetching data.");
+  }
+  //HINT: This is how you can use axios to do basic auth:
+  // https://stackoverflow.com/a/74632908
+  /*
+   axios.get(URL, {
+      auth: {
+        username: "abc",
+        password: "123",
+      },
+    });
+  */
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${API_URL}filter`,{
+        params: {
+          score:5,
+          apiKey: yourAPIKey,
+        },
+      }
+    );
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data.");
+  }
+});
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}secrets/42`, {
+      headers: {
+        Authorization: `Bearer ${yourBearerToken}`,
+      }
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data.");
+  }
+  // https://stackoverflow.com/a/52645402
+  /*
+  axios.get(URL, {
+    headers: { 
+      Authorization: `Bearer <YOUR TOKEN HERE>` 
+    },
+  });
+  */
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ 5.4 API Authentication/solution.js
@@ -0,0 +1,73 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+// TODO: Replace the values below with your own before running this file.
+const yourUsername = "gadisa";
+const yourPassword = "@gadisa";
+const yourAPIKey = "386a2873-35ee-4623-b092-783d51de0384";
+const yourBearerToken = "3182d35c-8c98-4e9b-bb02-21add9dd0f79";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(
+      API_URL + "/all?page=2",
+      {
+        auth: {
+          username: yourUsername,
+          password: yourPassword,
+        },
+      }
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/secrets/2", config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
