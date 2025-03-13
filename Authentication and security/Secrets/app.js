b206e0cee3f4ac88a8e53b52788a5090aba4b2d3
Gadisa Yusuf Aliyi <gadisa672@gmail.com>
2025-03-13 16:05:31 +0300

    level 2 authentication -hashing

--- Authentication and security/Secrets/app.js
+++ Authentication and security/Secrets/app.js
@@ -6,26 +6,34 @@ import bcrypt from "bcrypt";
 
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost:27017/test");
-const userSchema = {
-  email: String,
-  password: String,
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/test"),
+      console.log("Connected to MongoDB");
+  } catch {
+    console.log("mongodb.connect error occurred:", err.message);
+  }
 };
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+connectDB();
 const User = new mongoose.model("User", userSchema);
 app.get("/", (req, res) => res.render("home"));
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
 app.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: hashed,
   });
   try {
     const user = await newUser.save();
@@ -38,16 +46,24 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-
   try {
-    const user = await User.findOne({ email: username});
-    if (user.password===password) {
+    const user = await User.findOne({ email: username });
+    if (!user) {
+      res
+        .status(401)
+        .render("login", { message: "Invalid username or password" });
+      console.log("invalid username or password");
+      return;
+    }
+    const result = await bcrypt.compare(password, user.password);
+    console.log(result);
+    if (result) {
       res.render("secrets");
     } else {
       res
         .status(401)
         .render("login", { message: "Invalid username or password" });
-        console.log("invalid username or password");
+      console.log("invalid username or password");
     }
   } catch (err) {
     console.log(err);
